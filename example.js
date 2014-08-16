7aeb172958900c8862cd287627abdb9cc25c934f
Rouan van der Ende <rouan@8bo.org>
2014-08-16 09:27:26 +0200

    added example and documentation

---
+++ example.js
@@ -0,0 +1,20 @@
+var mailbot = require('./lib/email')
+
+// set to true to enable debug logging
+// this is useful to see if the remote server responds with some kind of error message
+mailbot.debug = false;	
+
+// construct the email
+var email = {}
+email.from = "noreply@yourdomain.com";		// Your email address. ex. "you@yourdomain.com";
+email.fromname = "Mailbot";					// Your name ex. "John Doe"
+
+email.rcpt = "santa@northpole.com";			// The target email address ex. "santa@northpole.com"
+email.rcptname = "Santa Claus";				// The target person's name ex. "Santa Claus"
+
+email.subject = "Debug test four";			// The short subject line of the email. "Email Subject"
+email.body = "last test";					// The main part of the email. "email message body content\r\nAnother line of content"
+
+mailbot.sendemail(email, function (data) {
+	console.log("EMAIL SENT")
+})
